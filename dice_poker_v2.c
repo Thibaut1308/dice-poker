28236a5cbc2c0b2a43045f0c2f896424159eb414
Thibaut Wojciak <thibaut.wojciak@telecomnancy.eu>
2022-01-28 15:41:23 +0100

    Ajout possibilité modifier le nombre de dés par manche

--- dice_poker_v2.c
+++ dice_poker_v2.c
@@ -2,14 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #define NB_MAINS 5
 #define NB_FACES 6
-#define NB_TIRAGES_MAX 25
+#define NB_TIRAGES_MAX 12
 
 
 /** Variable globale du nombre de tirages modifable en argument du programme*/
-int NB_TIRAGES =  5;
+int NB_TIRAGES =  7;
 
 typedef struct Joueur{
     char nom[50];
@@ -18,50 +19,13 @@ typedef struct Joueur{
     int scoreTotal;
 }Joueur;
 
-const char *combinaisons[] = {"Rien", "Paire", "Deux paires", "Trois d'un coup", "Straight", "House",
-                               "Quatre d'un coup", "Full"};
+const char *combinaisons[] = {"Rien", "Paire", "Triple", "Carre", "Penta", "Hexa", "Septa", "Octa",
+                               "Nona", "Deca", "Hendeca", "Dodeca"};
 
 /** Déclaration des joueurs */
 Joueur *joueur;
 Joueur *ordinateur;
 
-/**
- * contient() - Vérifie la présence d'un nombre dans un tableau
- * @num: Nombre à vérifier
- * @tab: Tableau dans lequel rechercher @num
- * Return:
- *  0: Le nombre n'a pas été trouvé
- *  1: Le nombre a été trouvé
- */
-int contient(int num, const int tab[NB_FACES]) {
-
-    int i;
-    for (i = 1; i < NB_FACES + 1; i++) {
-        if (tab[i] == num) {
-            return 1;
-        }
-    }
-    return 0;
-}
-
-/**
- * verifierNupletIdentiques() - Compte le nombre de n-uplet dans un tableau
- * @nuplet: N-uplet à compter
- * @tab: Tableau dans lequel compter les @nuplet
- * Return:
- *  res: Nombre de @nuplet trouvé dans @tab
- */
-int verifierNupletIdentiques(int nuplet, const int tab[NB_FACES]) {
-
-    int i;
-    int res = 0;
-    for (i = 1; i < NB_FACES + 1; i++) {
-        if (tab[i] == nuplet) {
-            res++;
-        }
-    }
-    return res;
-}
 
 /**
  * estConsecutif() - Vérifie si tous les chiffres du tableau sont consécutifs
@@ -105,10 +69,11 @@ int estConsecutif(const int tab[NB_TIRAGES_MAX]) {
     }
 }
 
-int identifie(const int hand[NB_TIRAGES_MAX]) {
+int* identifie(const int hand[NB_TIRAGES_MAX]) {
 
     /** On ignore l'indice zéro pour associé chaque indice à la valeur de la face du dé */
     int frequence[] = {0, 0, 0, 0, 0, 0, 0};
+    static int ffrequence[NB_TIRAGES_MAX];
     int i;
 
     /** Récupération des fréquences */
@@ -116,42 +81,14 @@ int identifie(const int hand[NB_TIRAGES_MAX]) {
         frequence[hand[i]]++;
     }
 
-    /** Cinq fois le même chiffre */
-    if (contient(5, frequence)) {
-        return 7;
+    /** Récupération des fréquences de fréquence */
+    for(i = 0;i < NB_TIRAGES + 1; i++) {
+        ffrequence[i] = 0;
     }
-
-    /** Quatre fois le même chiffre le dernier est forcément différent */
-    if (contient(4, frequence)) {
-        return 6;
+    for(i = 1;i < NB_FACES + 1; i++) {
+        ffrequence[frequence[i]]++;
     }
-
-    /** Trois fois le même chiffre */
-    if (contient(3, frequence)) {
-        /** Si deux derniers identiques "House" sinon "Trois d'un coup" */
-        if (contient(2, frequence)) {
-            return 5;
-        } else {
-            return 3;
-        }
-    }
-
-    /** Vérification deux paires ou paire simple */
-    int nombrePairesIdentiques = verifierNupletIdentiques(2, frequence);
-    if (nombrePairesIdentiques == 2) {
-        return 2;
-    } else {
-        if (nombrePairesIdentiques == 1)
-            return 1;
-    }
-
-    /** Cas d'un straight */
-    if (estConsecutif(hand)) {
-        return 4;
-    }else{
-        /** Cas d'une main avec tous les chiffres différents non consécutifs */
-        return 0;
-    }
+    return ffrequence;
 }
 
 /**
@@ -159,14 +96,31 @@ int identifie(const int hand[NB_TIRAGES_MAX]) {
  * @name: Nom du joueur
  * @hand: Main du joueur
  */
-void affiche(char *name, int hand[NB_TIRAGES_MAX]) {
+void affiche(char *name, const int hand[NB_TIRAGES_MAX]) {
     int i;
 
-    printf("%s: ", name);
+    printf("\n%s: ", name);
     for(i=0;i<NB_TIRAGES;i++) {
         printf("%d ", hand[i]);
     }
-    printf("(%s)\n", combinaisons[identifie(hand)]);
+    printf("( ");
+    int* ffrequence = identifie(hand);
+    for(i=2;i<NB_TIRAGES;i++){
+        int nombre = ffrequence[i];
+        if(nombre != 0) {
+            printf("%d %s ", nombre, combinaisons[i-1]);
+        }
+    }
+    printf(")");
+}
+
+int calculerPoints(const int* ffrequence) {
+    int i;
+    int total = 0;
+    for(i=2; i<NB_TIRAGES; i++) {
+        total += (int)(ffrequence[i]*pow(2, i));
+    }
+    return total;
 }
 
 int manche() {
@@ -181,8 +135,8 @@ int manche() {
     affiche(joueur->nom, joueur->mains[joueur->nextIndice]);
 
     /**  Calcul des points et détermination du gagnant */
-    int pointsJoueur = identifie(joueur->mains[joueur->nextIndice]);
-    int pointsOrdinateur = identifie(ordinateur->mains[joueur->nextIndice]);
+    int pointsJoueur = calculerPoints(identifie(joueur->mains[joueur->nextIndice]));
+    int pointsOrdinateur = calculerPoints(identifie(ordinateur->mains[joueur->nextIndice]));
 
     /** Incrémentation de l'indice des mains du joueur */
     joueur->nextIndice++;
@@ -192,15 +146,14 @@ int manche() {
     joueur->scoreTotal += pointsJoueur;
     ordinateur->scoreTotal += pointsOrdinateur;
 
+    printf("\n");
+
     if(pointsJoueur < pointsOrdinateur) {
-        printf("Victoire de l'ordinateur\n\n");
         return -1;
     }else{
         if(pointsJoueur > pointsOrdinateur) {
-            printf("Vous avez gagne !\n\n");
             return 1;
         }else{
-            printf("Egalite\n\n");
             return 0;
         }
     }
@@ -214,9 +167,14 @@ int manche() {
  *
  *  Initialise les joueurs, joue les manches et affiche les résultats.
  */
-int main() {
+int main(int argc, char *argv[]) {
 
     int i;
+    if(argc == 2) {
+        NB_TIRAGES = atoi(argv[1]);
+    }else{
+        return EXIT_FAILURE;
+    }
 
     /** Initialisation des joueurs  */
     joueur = malloc(sizeof (Joueur));
@@ -234,7 +192,7 @@ int main() {
         manche();
     }
 
-    printf("Points %s: %d\n", ordinateur->nom, ordinateur->scoreTotal);
+    printf("\nPoints %s: %d\n", ordinateur->nom, ordinateur->scoreTotal);
     printf("Points %s: %d\n", joueur->nom, joueur->scoreTotal);
 
 
@@ -247,6 +205,5 @@ int main() {
             printf("Egalite\n\n");
         }
     }
-
-    return 0;
+    return EXIT_SUCCESS;
 }
